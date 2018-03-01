cdc58720d91c69546c3a52c1e431e5567cbc03bd
Spazzy757 <brendankamp757@gmail.com>
2018-03-01 15:53:10 +0200

    Changed setup for pip install

--- kongoauth/authentication.py
+++ kongoauth/authentication.py
@@ -1,16 +1,18 @@
-from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from rest_framework import authentication
-from .helpers import get_redis
+from django.conf import settings
 from .user import TransientUser
-
+from .helpers import get_redis
 import json
+
 
 class KongOAuthAuthentication(authentication.BaseAuthentication):
 
     def authenticate(self, request):
-
-        ## note: this may need to be X_ (not HTTP_X_..) in real life:
+        """
+        Uses Kong's OAuth headers to create a a 'Transit' User
+        note: this may need to be X_ (not HTTP_X_..) in real life:
+        """
         anonymous = request.META.get('HTTP_X_ANONYMOUS_CONSUMER', None)
         if not anonymous:
             user_id = request.META.get('HTTP_X_AUTHENTICATED-USERID', None)
@@ -19,12 +21,14 @@ class KongOAuthAuthentication(authentication.BaseAuthentication):
         else:
             return AnonymousUser(), False
 
-    def get_user(self, user_id):
-        '''
-        '''
-        redis_key = getattr(settings, 'AUTH_REDIS_KEY', 'authorization.{}').format(user_id)
+    @staticmethod
+    def get_user(user_id):
+        """
+        Gets User information from a permission redis instance
+        """
+        redis_key = getattr(settings, 'AUTH_REDIS_KEY',
+                            'authorization.{}').format(user_id)
         redis = get_redis()
         user_data = json.loads(redis.get(redis_key))
-
         return TransientUser.from_redis_data(user_data)
 
--- kongoauth/helpers.py
+++ kongoauth/helpers.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 import redis
 
+
 def get_redis():
-    '''
+    """
     Get a redis connection
-    '''
     # rediss://:password@hostname:port/0
+    """
     defualt_connection = {
         "host": "redis"
     }
     defualt_connection.update(getattr(settings, 'REDIS_CONN', {}))
-    return redis.Redis(**{"host":"redis"})
+    return redis.Redis(**{"host": "redis"})
